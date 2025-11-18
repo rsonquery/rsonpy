463fea354f164a58aa09134602904b56dbde1cfd
Charles Paperman <charles.paperman@univ-lille.fr>
2025-11-18 08:37:04 +0100

    new CI llm generated

--- rsonpy/__init__.py
+++ rsonpy/__init__.py
@@ -1,10 +1,11 @@
 from rsonpy import rsonpy as rsonpath
 import json
 
+
 def loads(s: str, query: str, json_loader=json.loads):
     """
-        Parse the input string that is assumed to be a valid JSON-encoding and return an iterator
-        over solutions of the query over the document.
+    Parse the input string that is assumed to be a valid JSON-encoding and return an iterator
+    over solutions of the query over the document.
     """
     for start, end in rsonpath.loads(s, query):
         yield json.loads(s[start:end])
@@ -13,4 +14,3 @@ def loads(s: str, query: str, json_loader=json.loads):
 __doc__ = rsonpath.__doc__
 if hasattr(rsonpath, "__all__"):
     __all__ = rsonpath.__all__
-
